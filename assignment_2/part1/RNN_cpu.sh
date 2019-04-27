7253555b8b13740e256e3b67b5f10ebc14e1fd43
NILOIDE <nilstoltanso@gmail.com>
2019-04-28 01:56:47 +0200

    cpu job

---
+++ assignment_2/part1/RNN_cpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=convnet
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=02:30:50
+#sBATCH --mem=60000M
+
+module purge
+module load eb
+
+module load Python/3.6.3-foss-2017b
+module load cuDNN/7.0.5-CUDA-9.0.176
+module load NCCL/2.0.5-CUDA-9.0.176
+
+export LD_LIBRARY_PATH=/hpc/eb/Debian9/cuDNN/7.1-CUDA-8.0.44-GCCcore-5.4.0/lib64:$LD_LIBRARY_PATH
+
+srun python3 train.py
+
+
